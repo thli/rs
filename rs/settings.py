59f02e555fd57c459c420c2fa61005a09cccde77
thli <thli@andrew.cmu.edu>
2015-09-06 18:02:54 -0400

    created postgres db

--- rs/settings.py
+++ rs/settings.py
@@ -57,10 +57,17 @@ WSGI_APPLICATION = 'rs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+import dj_database_url
+DATABASES = {}
+# DATABASES['default'] =  dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rsgraphs',
+        'USER': 'thli',
+        'PASSWORD': 'hiphopopotamus45',
+        'HOST': 'localhost',
+        'PORT': '',  # Settoemptystringfordefault.
     }
 }
 
