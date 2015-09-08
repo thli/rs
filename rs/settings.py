917b5c0ef4b2702d001e45e15af443900c25d2b3
thli <thli@andrew.cmu.edu>
2015-09-08 19:20:56 -0400

    removed static dir and root

--- rs/settings.py
+++ rs/settings.py
@@ -93,9 +93,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
